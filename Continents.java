3aa31c41733ad587a644cede2756715205e8c978
Nick Zelada <nzela002@fiu.edu>
2020-08-14 13:23:21 -0400

    Add files via upload

---
+++ Continents.java
@@ -0,0 +1,44 @@
+public class Continents {
+  /*The largest city in that certain continet*/
+int continent;
+  public Continents(int areas){
+  continent = areas;
+  }
+  public void lookContinents(){
+    switch(continent)	{
+      case 1:
+             System.out.println("North America: Mexico City, Mexico\n"); 
+      break;
+      case 2:
+       System.out.println("South America: Sao Paulo, Brazil\n"); 
+      break;
+      case 3:
+       System.out.println("Europe: Moscow, Russia\n"); 
+      break;
+      case 4:
+       System.out.println("Africa: Lagos, Nigeria\n"); 
+      break;
+      case 5:
+       System.out.println("Asia: Shanghai, China\n"); 
+      break;
+      case 6:
+       System.out.println("Australia: Sydney, Australia\n"); 
+      break;
+      case 7:
+       System.out.println("Antarctica: McMurdo Station, US\n"); 
+      break;
+      default:
+      System.out.println("Undefined continent!\n");   
+     }
+  }
+	public static void main(String[] args) {
+    Continents num = new Continents(4);
+    num.lookContinents();
+    Continents num1 = new Continents(5);
+    num1.lookContinents();
+    Continents num2 = new Continents(9);
+    num2.lookContinents();
+	
+
+	}
+}
